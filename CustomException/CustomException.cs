841b9c35f8b41b6df6e2ca55ff5965a12a5e7a4b
Egbujie Chizoba <egbujie.chizoba@gmail.com>
2022-11-24 10:14:05 +0100

    Add files via upload

---
+++ CustomException/CustomException.cs
@@ -0,0 +1,35 @@
+using System;
+namespace CustomExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1, Number2, Result;
+            try
+            {
+                Console.WriteLine("Enter First Number:");
+                Number1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Second Number:");
+                Number2 = int.Parse(Console.ReadLine());
+
+                if (Number2 % 2 == 0)
+                {
+                    throw new EvenNumberException("Even Number Exception Occured Inside the Main Method of Program Class");
+                }
+                Result = Number1 / Number2;
+                Console.WriteLine(Result);
+            }
+            catch (EvenNumberException one)
+            {
+                Console.WriteLine($"Message: {one.Message}");
+                // Console.WriteLine($"HelpLink: {one.HelpLink}");
+                Console.WriteLine($"Source: {one.Source}");
+                Console.WriteLine($"StackTrace: {one.StackTrace}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomException/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CustomExceptionHandling
+{
+    //Creating our own Exception Class by inheriting Exception class
+    public class EvenNumberException : Exception
+    {
+        public EvenNumberException()
+        {
+        }
+        public EvenNumberException(string message)
+            : base(message)
+        {
+        }
+        public EvenNumberException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+      
+       
+    }
+}
